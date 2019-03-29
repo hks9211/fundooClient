3cdd81cede118fca027e160f35841fb97995d8f0
honeykrs <honeykrsingh16@gmail.com>
2019-03-29 15:23:11 +0530

    updates

--- src/app/components/login-user/login-user.component.ts
+++ src/app/components/login-user/login-user.component.ts
@@ -35,6 +35,8 @@ export class LoginUserComponent implements OnInit {
     ]);
 
   ngOnInit() {
+    this.spinnerService.show();
+
   }
 
   errorMessageForEmail() {
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -7,11 +7,11 @@ import { UserModel } from '../model/userModel'
   providedIn: 'root'
 })
 export class HttpService {
-  private url = 'http://18.222.210.103:3000/';
+  private url = 'http://localhost:3000/';
   constructor(
     private http: HttpClient
   ) { }
-
+  // 18.222.210.103
   register : UserModel = {
     firstName : "",
     lastName : "",
